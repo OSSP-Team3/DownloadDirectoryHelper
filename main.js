ef89fdd93e3b9670fef02ce087938d1b4cb24cd0
jimin9038 <hajimin6241@gmail.com>
2021-05-13 19:19:50 +0900

    feature-getting file list in download developed

--- main.js
+++ main.js
@@ -30,6 +30,14 @@ app.whenReady().then(() => {
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+  app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    });
+  });
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
