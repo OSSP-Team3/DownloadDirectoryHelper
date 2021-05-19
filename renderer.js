901d0d81291a2b1eba1debe2f7d3bc40eb93175d
jimin9038 <hajimin6241@gmail.com>
2021-05-19 15:37:43 +0900

    Erase unnecessary comment & fix typo

--- renderer.js
+++ renderer.js
@@ -9,13 +9,6 @@ document.getElementById('getFilelist').addEventListener('click', (evt)=> {
   evt.preventDefault();
   console.log("Load Files List");
   window.electron.showDownloadFiles();
-    // fs.readdir(testFolder, (err, files) => {
-    //     console.log("asdf");
-    //     files.forEach(file => {
-    //         var tag = document.createElement("p").appendChild(document.createTextNode(file));
-    //         document.getElementById("downloadFiles").appendChild(tag);
-    //     });
-    // });
 });
 
 document.getElementById('sortByDate').addEventListener('click', (evt)=> {
